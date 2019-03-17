d467f12f0a87a411e77bcf2f66584565de1701e2
KuriyamaMirai <lovef@outlook.com>
2019-03-17 16:33:27 +0800

    1

---
+++ src/evetool.core/Express/Express.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using evetool.core.model;
+using Microsoft.EntityFrameworkCore;
+
+namespace evetool.core.Express
+{
+    public class Express : CoreBase
+    {
+        public Express(EVEToolDbContext db) : base(db)
+        {
+        }
+
+        public ExpressFeeOption GetExpressOption(int id)
+        {
+            this.DbContext.
+        }
+    }
+}
---
+++ src/evetool.entity/Express/ExpressFeeOption.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace evetool.core.entity
+{
+    /// <summary>
+    /// 运费配置
+    /// </summary>
+    public class ExpressFeeOption
+    {
+        public int ID { get; set; }
+        /// <summary>
+        /// 运费单价
+        /// </summary>
+        public decimal UnitPrice { get; set; }
+        /// <summary>
+        /// 体积递增
+        /// </summary>
+        public double IncrementVolume { get; set; }
+        /// <summary>
+        /// 体积上下浮动
+        /// </summary>
+        public double FloatingVolume { get; set; }
+        /// <summary>
+        /// 免费体积
+        /// </summary>
+        public double FreeVolume { get; set; }
+        /// <summary>
+        /// 是否使用保险
+        /// </summary>
+        public bool IsCare { get; set; }
+        /// <summary>
+        /// 保险配置
+        /// </summary>
+        public ExpressCareOption Care { get; set; }
+    }
+}
